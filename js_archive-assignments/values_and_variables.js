73c3260aa1e40e3a2ac5850453277e0045f42b34
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-03 00:03:27 +0530

    completed assignment - values and variables

--- js_archive-assignments/values_and_variables.js
+++ js_archive-assignments/values_and_variables.js
@@ -1,3 +1,7 @@
 var country = "India";
 var continent = "Asia";
 var population = "138 crores";
+
+console.log("Country : " + country);
+console.log("Continent : " + continent);
+console.log("Population : " + population);
