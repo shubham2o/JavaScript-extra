1e56f19dd93b4c6a413e4839ea7020339f71f623
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-03 20:01:27 +0530

    conditional ternary operator added

--- conditional_(ternary)_operator/conditional_ternary_operator.js
+++ conditional_(ternary)_operator/conditional_ternary_operator.js
@@ -8,3 +8,8 @@ const vote = age >= 18 ?
 `Eligible to vote` : 
 `Not eligible to vote`;
 console.log(vote);
+
+console.log(`${age >= 18 ? 
+    `You are eligible to drive a vehicle` : 
+    `You are not eligible to drive a vehicle`}`
+);
