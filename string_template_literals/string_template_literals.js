6912660cf84a977a4cbf5503010db41c4dee3743
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-03 10:08:29 +0530

    string template literals added

---
+++ string_template_literals/string_template_literals.js
@@ -0,0 +1,9 @@
+const firstName = "Shubham";
+const job = "Student";
+const birthYear = 2000;
+const year = 2037;
+
+const shubham = 
+"I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+
+console.log(shubham);
