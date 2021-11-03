fad6f9fe82d3658c9f08b7488900a8685f36f1a3
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-03 14:28:44 +0530

    ===Fixed~adding Number() toConvert it into aNumber

--- ==vs===/==vs===.js
+++ ==vs===/==vs===.js
@@ -27,3 +27,19 @@ if (favourite2 === 5) {
 else {
     console.log(`False!(===)`);
 }
+
+
+
+// FIXED!! ~ Tripple equals to operator by adding 'Number' to it
+// (===)
+const favourite3 = Number(prompt(`What's your favourite number BETWEEN 1-10? ~ Fixed`));
+
+console.log(favourite3);
+console.log(typeof favourite3);
+
+if (favourite3 === 5) {
+    console.log(`Cool! 5 is an amazing number ~ True!(===) ~ Fixed`);
+}
+else {
+    console.log(`False!(===) ~ Fixed`);
+}
