32ebb273c4bf2ee8a6dd4d46df6f66ac69e722d7
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-03 15:46:21 +0530

    Dolphins and Koalas Data added to the js file

---
+++ js_archive-coding_challenges/coding_challenge3.js
@@ -0,0 +1,45 @@
+// DOLPHINS
+let dolphinsData1_1 = 96; 
+let dolphinsData1_2 = 108; 
+let dolphinsData1_3 = 89;
+
+let avgDolphinsData1 = (dolphinsData1_1 + dolphinsData1_2 + dolphinsData1_3) / 3;
+console.log(`1> avgDolphinsData1 ~ ${avgDolphinsData1}`);
+
+let dataBonus1Dolphins1 = 97;
+let dataBonus1Dolphins2 = 112;
+let dataBonus1Dolphins3 = 101;
+
+let avgDataBonus1Dolphins = (dataBonus1Dolphins1 + dataBonus1Dolphins2 + dataBonus1Dolphins3) / 3;
+console.log(`2> avgDataBonus1Dolphins ~ ${avgDataBonus1Dolphins}`);
+
+let dataBonus2Dolphins1 = 97;
+let dataBonus2Dolphins2 = 112;
+let dataBonus2Dolphins3 = 101;
+
+let avgDataBonus2Dolphins = (dataBonus2Dolphins1 + dataBonus2Dolphins2 + dataBonus2Dolphins3) / 3;
+console.log(`3> avgDataBonus2Dolphins ~ ${avgDataBonus2Dolphins}`);
+
+
+
+// KOALAS
+let koalasData1_1 = 88; 
+let koalasData1_2 = 91; 
+let koalasData1_3 = 110;
+
+let avgKoalasData1 = (koalasData1_1 + koalasData1_2 + koalasData1_3) / 3;
+console.log(`4> avgKoalasData1 ~ ${avgKoalasData1}`);
+
+let dataBonus1Koalas1 = 109;
+let dataBonus1Koalas2 = 95;
+let dataBonus1Koalas3 = 123;
+
+let avgDataBonus1Koalas = (dataBonus1Koalas1 + dataBonus1Koalas2 + dataBonus1Koalas3) / 3;
+console.log(`5> avgDataBonus1Koalas ~ ${avgDataBonus1Koalas}`);
+
+let dataBonus2Koalas1 = 109;
+let dataBonus2Koalas2 = 95;
+let dataBonus2Koalas3 = 106;
+
+let avgDataBonus2Koalas = (dataBonus2Koalas1 + dataBonus2Koalas2 + dataBonus2Koalas3) / 3;
+console.log(`6> avgDataBonus2Koalas ~ ${avgDataBonus2Koalas}`);
