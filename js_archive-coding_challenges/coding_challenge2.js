b802bd5fa369c24e724ff86928697213412b6ab6
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-03 11:10:43 +0530

    Test Data 2 passed and coding challenge2 completed

--- js_archive-coding_challenges/coding_challenge2.js
+++ js_archive-coding_challenges/coding_challenge2.js
@@ -15,3 +15,23 @@ if (markBMI > johnBMI) {
 else {
     console.log(`John's BMI is higher than Mark's by ${johnBMI - markBMI}!`);
 }
+
+
+
+// TEST 
+// DATA 
+// 2
+const markMass2 = 95;
+const markHeight2 = 1.88;
+const johnMass2 = 85;
+const johnHeight2 = 1.76;
+
+const markBMI2 = markMass2 / markHeight2 ** 2;
+const johnBMI2 = johnMass2 / (johnHeight2 * johnHeight2);
+
+if (markBMI2 > johnBMI2) {
+    console.log(`Mark's BMI is higher than John's by ${markBMI2 - johnBMI2}!`);
+}
+else {
+    console.log(`John's BMI is higher than Mark's by ${johnBMI2 - markBMI2}!`);
+}
