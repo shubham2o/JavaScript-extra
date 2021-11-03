0a516873fb0c4b1369dde07430eee152f0983870
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-03 09:31:30 +0530

    markBMI2 added

--- js_archive-coding_challenges/coding_challenge1.js
+++ js_archive-coding_challenges/coding_challenge1.js
@@ -1,3 +1,6 @@
+// TEST 
+// DATA 
+// 1
 let markMass = 78;
 let markHeight = 169;
 let johnMass = 92;
@@ -11,3 +14,16 @@ console.log(johnBMI)
 
 let markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
+
+
+// TEST
+// DATA
+// 2
+
+let markMass2 = 95;
+let markHeight2 = 188;
+let johnMass2 = 85;
+let markHieght2 = 176;
+
+let markBMI2 = markMass2 / markHeight2 ** 2 / (markHeight2 * markHeight2);
+console.log(markBMI2);
