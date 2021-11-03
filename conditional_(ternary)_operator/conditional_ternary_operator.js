0f37c5c8edc9003454d1daf0e7e8acd5b9b9ac7f
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-03 19:52:16 +0530

    conditional ternary operator added

--- conditional_(ternary)_operator/conditional_ternary_operator.js
+++ conditional_(ternary)_operator/conditional_ternary_operator.js
@@ -1,2 +1,5 @@
 const age = 23;
 age >= 18 ? console.log(`I like to drink wine 🍷`) : console.log(`I like to drink water 💦`);
+
+const vote = age >= 18 ? `Eligible to vote` : `Not eligible to vote`;
+console.log(vote);
