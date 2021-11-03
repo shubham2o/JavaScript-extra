bda7388d688b0256dbeba40183985f5c3dcb2899
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-03 20:34:26 +0530

    coding challenge 4 completed

---
+++ js_archive-coding_challenges/coding_challenge4.js
@@ -0,0 +1,54 @@
+// DATA 1
+// Bill Value is 275
+let bill1 = 275;
+let tip1;
+let total1;
+
+if (bill1 >= 50 && bill1 <= 300) {
+    tip1 = (15 / 100) * bill1;
+    total1 = bill1 + tip1;
+    console.log(`1> The bill was ${bill1}, the tip was ${tip1}, and the total amount to be paid ${total1}.`);
+}
+else {
+    tip1 = (20 / 100) * bill1;
+    total1 = bill1 + tip1;
+    console.log(`1> The bill was ${bill1}, the tip was ${tip1}, and the total amount to be paid ${total1}.`);
+}
+
+
+
+// DATA 2
+// Bill Value is 40
+let bill2 = 40;
+let tip2;
+let total2;
+
+if (bill2 >= 50 && bill2 <= 300) {
+    tip2 = (15 / 100) * bill2;
+    total2 = bill2 + tip2;
+    console.log(`2> The bill was ${bill2}, the tip was ${tip2}, and the total amount to be paid ${total2}.`);
+}
+else {
+    tip2 = (20 / 100) * bill2;
+    total2 = bill2 + tip2;
+    console.log(`2> The bill was ${bill2}, the tip was ${tip2}, and the total amount to be paid ${total2}.`);
+}
+
+
+
+// DATA 3
+// Bill Value is 430
+let bill3 = 430;
+let tip3;
+let total3;
+
+if (bill3 >= 50 && bill3 <= 300) {
+    tip3 = (15 / 100) * bill3;
+    total3 = bill3 + tip3;
+    console.log(`3> The bill was ${bill3}, the tip was ${tip3}, and the total amount to be paid ${total3}.`);
+}
+else {
+    tip3 = (20 / 100) * bill3;
+    total3 = bill3 + tip3;
+    console.log(`3> The bill was ${bill3}, the tip was ${tip3}, and the total amount to be paid ${total3}.`);
+}
