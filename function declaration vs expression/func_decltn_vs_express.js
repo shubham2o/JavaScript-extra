ee6916b6bede75d833a84225350ff5f805308dfd
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-04 08:02:08 +0530

    comments added

--- function declaration vs expression/func_decltn_vs_express.js
+++ function declaration vs expression/func_decltn_vs_express.js
@@ -7,9 +7,31 @@ console.log(age);
 
 
 
-// Function Expression
-const calcAge2 = function (birthYear2) {
+/* const age2 declared before function
+and will work normally like before 
+and give the correct output */
+const age2 = calcAge2(2000);
+function calcAge2(birthYear2) {
     return 2037 - birthYear2;
 }
-const age2  = calcAge2(2000);
 console.log(age2);
+
+
+
+// Function Expression
+const calcAge3 = function (birthYear3) {
+    return 2037 - birthYear3;
+}
+const age3  = calcAge3(2000);
+console.log(age3);
+
+
+
+/* const age4 declared before function
+and will not work normally like before 
+and will give error as an output */
+const age4 = calcAge4(2000);
+const calcAge4 = function(birthYear4) {
+    return 2037 - birthYear4;
+}
+console.log(age4);
