6e26f0bd17f2bf6d28bbf1834dff1e4bd427539f
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-04 09:02:16 +0530

    functions calling other functions added

---
+++ functions calling other functions/func_calling_other_func.js
@@ -0,0 +1,14 @@
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor(apple, orange) {
+    const applePieces = cutFruitPieces(apple);
+    const orangePieces = cutFruitPieces(orange);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+const mixJuice = fruitProcessor(1, 2);
+console.log(mixJuice);
