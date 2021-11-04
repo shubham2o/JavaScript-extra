6ce04f4e62911550dfc6412b4e12daf03b5f3a64
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-04 08:22:42 +0530

    arrow function added

--- arrow function/arrow_function.js
+++ arrow function/arrow_function.js
@@ -11,3 +11,13 @@ console.log(age);
 const calcAge1 = birthYear1 => 2037 - birthYear1; 
 const age1 = calcAge1(2000);
 console.log(age1);
+
+
+
+const yearsUntilRetirement = birthYear2 => {
+    const age2 = 2037 - birthYear2;
+    const retirementAge = 65 - age2;
+    return retirementAge;
+}
+const retirement = yearsUntilRetirement(2000);
+console.log(retirement);
