bccbcac0ec3d243468d12feaf8f5fdad22a0f74c
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-04 08:31:04 +0530

    arrow function added

--- arrow function/arrow_function.js
+++ arrow function/arrow_function.js
@@ -8,7 +8,8 @@ console.log(age);
 
 
 // Arrow Function
-const calcAge1 = birthYear1 => 2037 - birthYear1; 
+const calcAge1 = birthYear1 => 
+2037 - birthYear1; 
 const age1 = calcAge1(2000);
 console.log(age1);
 
@@ -21,3 +22,15 @@ const yearsUntilRetirement = birthYear2 => {
 }
 const retirement = yearsUntilRetirement(2000);
 console.log(retirement);
+
+
+
+const yearsUntilRetirement1 = (birthYear3, firstName) => {
+    const age3 = 2037 - birthYear3;
+    const retirementAge1 = 65 - age3;
+    return `${firstName} retires in ${retirementAge1} years.`;
+}
+const retirement1 = yearsUntilRetirement1(2000, "Shubham");
+const retirement2 = yearsUntilRetirement1(1988, "Stephen");
+console.log(retirement1);
+console.log(retirement2);
