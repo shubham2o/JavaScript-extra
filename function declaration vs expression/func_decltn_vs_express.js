ed847d30f9d8a66e202d8962ce8204ff010837d0
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-04 07:50:52 +0530

    function expression added

--- function declaration vs expression/func_decltn_vs_express.js
+++ function declaration vs expression/func_decltn_vs_express.js
@@ -3,9 +3,13 @@ function calcAge(birthYear) {
     return 2037 - birthYear;
 }
 const age = calcAge(2000);
-
 console.log(age);
 
 
 
 // Function Expression
+const calcAge2 = function (birthYear2) {
+    return 2037 - birthYear2;
+}
+const age2  = calcAge2(2000);
+console.log(age2);
