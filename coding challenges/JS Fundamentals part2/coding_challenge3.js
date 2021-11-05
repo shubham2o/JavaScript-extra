b569427d7420d5dbcdef7e9572c321c0ae5e5c07
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-05 20:07:55 +0530

    John Smith's bmi added

--- coding challenges/JS Fundamentals part2/coding_challenge3.js
+++ coding challenges/JS Fundamentals part2/coding_challenge3.js
@@ -10,3 +10,18 @@ const markMiller = {
     }
 };
 console.log(markMiller.calcBMI());
+
+
+
+// JOHN SMITH'S
+// BMI
+const johnSmith = {
+    fullName : 'John Smith',
+    mass : 92,
+    height : 1.95,
+    calcBMI : function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+};
+console.log(johnSmith.calcBMI());
