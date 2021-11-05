cd46df6965adcafda3bf2269b99dc3f8739abb80
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-05 13:56:59 +0530

    dot vs bracket notation.js added

---
+++ dot. vs bracket[] Notation/dot_vs_bracket_notation.js
@@ -0,0 +1,31 @@
+const shubham = {
+    firstName : 'Shubham',
+    lastName : 'Sharma',
+    age : 2037 - 2000,
+    job : 'student',
+    friends : ['Stephen' , 'Klay', 'Dray']
+};
+console.log(shubham);
+
+
+// DOT notation
+console.log(shubham.firstName);
+console.log(shubham.lastName);
+console.log(shubham.age);
+console.log(shubham.job);
+console.log(shubham.friends);
+console.log(shubham.friends[0]);
+console.log(shubham.friends[1]);
+console.log(shubham.friends[2]);
+
+
+// BRACKET notation
+console.log(shubham['firstName']);
+console.log(shubham['lastName']);
+console.log(shubham['age']);
+console.log(shubham['job']);
+console.log(shubham['friends']);
+
+const monkey = 'Name';
+console.log(shubham['first' + monkey]);
+console.log(shubham['last' + monkey]);
