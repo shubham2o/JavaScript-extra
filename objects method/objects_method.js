0845bab9a5e91519df8210faed8896efaec1e4f9
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-05 15:37:12 +0530

    objects method added

--- objects method/objects_method.js
+++ objects method/objects_method.js
@@ -5,9 +5,28 @@ const shubham = {
     job : 'Student',
     friends : ['Stephen', 'Klay', 'Dray'],
     hasDriversLicense : true,
-
     calcAge : function(birthYear) {
         return 2037 - 2000;
     }
 };
+
 console.log(shubham.calcAge());
+
+
+
+const shubham1 = {
+    oneFirstName : 'Shubham',
+    oneLastName : 'Sharma',
+    oneBirthYear : 2000,
+    oneJob : 'Student',
+    oneFriends : ['Stephen', 'Klay', 'Dray'],
+    oneHasDriversLicense : true,
+    oneCalcAge : function() {
+        console.log(this);
+        console.log(this.oneFirstName);
+        console.log(this.oneLastName);
+        return 2037 - this.birthYear;
+    }
+};
+
+console.log(shubham1.oneCalcAge());
