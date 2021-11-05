153a282e5d8374c7dc78372146f5ffd7540f503d
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-05 20:16:13 +0530

    comparison done & coding challenge 3 completed

--- coding challenges/JS Fundamentals part2/coding_challenge3.js
+++ coding challenges/JS Fundamentals part2/coding_challenge3.js
@@ -31,6 +31,6 @@ console.log(`2> John Smith's BMI is ${johnSmith.calcBMI()}...`);
 // Who has higher BMI among 
 // these two? compare!
 console.log(`3> ${markMiller.calcBMI() > johnSmith.calcBMI() ? 
-    `Mark Miller's BMI (${markMiller.calcBMI()}) is higher than John Smith's (${johnSmith.calcBMI()})` : 
-    `John Smith's BMI (${johnSmith.calcBMI()}) is higher than Mark Miller's (${markMiller.calcBMI()})`}`
+    `Mark Miller's BMI (${markMiller.calcBMI()}) is higher than John Smith's (${johnSmith.calcBMI()})!` : 
+    `John Smith's BMI (${johnSmith.calcBMI()}) is higher than Mark Miller's (${markMiller.calcBMI()})!`}`
 );
