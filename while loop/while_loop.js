0eb2d49931441fbcab3a58279c689f18e0cf4197
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-06 08:38:35 +0530

    while loop added

---
+++ while loop/while_loop.js
@@ -0,0 +1,12 @@
+// for loop
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`FOR LOOP ~ Lifting weights repitition ${rep}`);
+}
+
+
+// while loop
+let reps = 1;
+while (reps <= 10) {
+    console.log(`WHILE LOOP ~~~~~~~~~~~~ Lifting weights repitition ${reps}`);
+    reps++;
+}
