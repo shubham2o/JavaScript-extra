14b1b4b801f594f7e9af1e797dda7e408d593232
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-06 23:57:27 +0530

    let, const, var added + assignment completed

--- assignments/JS Fundamentals part1/let,const,var.js
+++ assignments/JS Fundamentals part1/let,const,var.js
@@ -1,5 +1,14 @@
-language = "english";
-const country = "India";
-const continent = "Asia";
-const isIsland = false;
+let country = 'India';
+let continent = 'Asia';
+let population = '138 crores';
+let isIsland = false;
+let language = 'Hindi';
+
+language = 'English';
 isIsland = true;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
