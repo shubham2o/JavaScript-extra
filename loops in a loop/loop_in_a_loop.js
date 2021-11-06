ada78c3f9423fdc9abfcf1f94e3c8fbf8aa1eed0
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-06 08:15:31 +0530

    looping backwards using an array added

---
+++ loops in a loop/loop_in_a_loop.js
@@ -0,0 +1,13 @@
+// Looping Backwards 
+// using an array.
+const shubham = [
+    'Shubham',
+    'Sharma',
+    2037 - 2000,
+    'student',
+    ['Stephen', 'Klay', 'Dray']
+];
+
+for (let i = shubham.length - 1; i >= 0; i--) {
+    console.log(i, shubham[i]);
+}
