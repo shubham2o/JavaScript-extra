c5a19591fe89ca3be47d86f0ff5e80b4dd700fb8
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-06 08:27:26 +0530

    loop in a loop added

--- loops in a loop/loop_in_a_loop.js
+++ loops in a loop/loop_in_a_loop.js
@@ -16,9 +16,11 @@ for (let i = shubham.length - 1; i >= 0; i--) {
 
 // Loop in a loop
 for (let exercise = 1; exercise < 4; exercise++) {
-    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~ Starting exercise ${exercise}`);
+    console.log(`
+Starting Exercise ${exercise} ~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
 
     for (let rep = 1; rep < 6; rep++) {
-        console.log(`Lifting weight repetition ${rep}`);
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
     }
 };
