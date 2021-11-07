83c33ea9107e8dd6f5b1cc22b3909880b7582cb5
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-07 09:27:58 +0530

    basic operator added and assignment completed

---
+++ assignments/JS Fundamentals part1/basic_operators.js
@@ -0,0 +1,19 @@
+let population = 1;
+let country = "India";
+let continent = "Asia";
+let language = "Hindi";
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description1 = 
+country + 
+' is in ' + 
+continent + 
+', and its ' + 
+population +
+' billion people speak ' + 
+language;
+console.log(description1);
