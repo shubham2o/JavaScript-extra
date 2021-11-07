cecbc66db5c569e0be713adacb3336b7ff4f8b01
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-07 09:17:27 +0530

    type conversion and coersion assignment completed

--- assignments/JS Fundamentals part1/type_conversion&coersion.js
+++ assignments/JS Fundamentals part1/type_conversion&coersion.js
@@ -1,1 +1,9 @@
+const x = '9' - '5';
+console.log(typeof x);
 console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+const y = "9" - "5";
+console.log(typeof y);
