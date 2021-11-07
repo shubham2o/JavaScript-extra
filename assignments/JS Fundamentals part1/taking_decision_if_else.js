745ec9ca9e2cacd7a4d5376606538bd97f0b5fbf
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-07 23:12:59 +0530

    assignment completed

---
+++ assignments/JS Fundamentals part1/taking_decision_if_else.js
@@ -0,0 +1,9 @@
+let population = 140;
+const country = "India"; 
+
+if (population > 100) {
+    console.log(`'${country}'s population is above the average'.`);
+}
+else {
+    console.log(`'${country}'s population is below the average'.`)
+}
