48928ea956bd4978926e9c28710fbba286315c00
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-07 23:48:40 +0530

    Assignment completed

---
+++ assignments/JS Fundamentals part1/logical_operators.js
@@ -0,0 +1,10 @@
+const language = "English";
+let population = "50";
+const isIsland = false;
+
+if (language === "English" && population <= 50 && !isIsland) {
+    console.log(`You should live in Portugal.`);
+}
+else {
+    console.log(`Portugal does not meet your criteria.`);
+}
