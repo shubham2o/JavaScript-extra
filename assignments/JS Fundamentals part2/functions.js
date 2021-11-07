c98f043f6cbf1aec37e5420dce678fd6d15a70a9
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-08 00:55:11 +0530

    Assignment completed

---
+++ assignments/JS Fundamentals part2/functions.js
@@ -0,0 +1,7 @@
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its captal city is ${capitalCity} :)`;
+}
+
+console.log(`~> ${describeCountry("Portugal", 10, "Lisbon")}`);
+console.log(`~> ${describeCountry("Finland", 6, "Helsinki")}`);
+console.log(`~> ${describeCountry("Germany", 83, "Berlin")}`);
