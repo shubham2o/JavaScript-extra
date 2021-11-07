1fe760eb6d633c19f9370481aa831406863fd3f4
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-07 22:59:01 +0530

    string template literals and assignment completed

---
+++ assignments/JS Fundamentals part1/strings_template_literals.js
@@ -0,0 +1,15 @@
+let population = 140;
+const country = 'India';
+const continent = 'Asia';
+const language = 'Hindi';
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+const description =
+`${country} is in ${continent}, and its ${population} crores people speak ${language}.`;
+
+console.log(description);
