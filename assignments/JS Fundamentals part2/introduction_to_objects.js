a76d80a25b0f38ff48f6899de3036dab465ec6eb
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-08 10:31:42 +0530

    Assignment Completed

---
+++ assignments/JS Fundamentals part2/introduction_to_objects.js
@@ -0,0 +1,26 @@
+let myCountry = {
+    country : "India",
+    capital : "Delhi",
+    language : "Hindi",
+    population : "1380",
+    neighbours : [
+        "Pakistan",
+        "China",
+        "Nepal",
+        "Afghanistan",
+        'Sri Lanka'
+    ]
+};
+
+console.log(myCountry);
+console.log(myCountry.country);
+console.log(myCountry.capital);
+console.log(myCountry.language);
+console.log(myCountry.population + " million");
+console.log(myCountry.neighbours);
+console.log(myCountry.neighbours.length);
+console.log(myCountry.neighbours[0]);
+console.log(myCountry.neighbours[1]);
+console.log(myCountry.neighbours[2]);
+console.log(myCountry.neighbours[3]);
+console.log(myCountry.neighbours[4]);
