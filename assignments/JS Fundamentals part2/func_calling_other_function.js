c567181a12086228d2d9252001b89978e69254fc
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-08 08:53:34 +0530

    Assignment Completed

--- assignments/JS Fundamentals part2/func_calling_other_function.js
+++ assignments/JS Fundamentals part2/func_calling_other_function.js
@@ -1,20 +1,20 @@
-function percentageOfWorld1(countryPop) {
-    return (countryPop / 7900) * 100;
+function percentageOfWorld1(countryPop, worldPop) {
+    return (countryPop / worldPop) * 100;
 }
 
-console.log(`${percentageOfWorld1(1441)}
+console.log(`${percentageOfWorld1(1441, 7900)}%
 `);
 
 
 
 // Function calling other function
-function describePopulation(country, population) {
-    let percentage = percentageOfWorld1(population);
+function describePopulation(country, population, world) {
+    let percentage = percentageOfWorld1(population, world);
     let description = `${country} has ${population} million people, which is about ${percentage}% of the world.
 `;
     return description;
 }
 
-console.log(`${describePopulation("Portugal", 10)}`);
-console.log(`${describePopulation("China", 1441)}`);
-console.log(`${describePopulation("USA", 332)}`);
+console.log(`${describePopulation("Portugal", 10, 7900)}`);
+console.log(`${describePopulation("China", 1441, 7900)}`);
+console.log(`${describePopulation("USA", 332, 7900)}`);
