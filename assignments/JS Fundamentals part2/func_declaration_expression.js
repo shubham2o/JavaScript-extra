eb65b8b4c1b62a38d0b3eae2d24b9643c02b09cd
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-08 07:40:42 +0530

    Assignment Completed

--- assignments/JS Fundamentals part2/func_declaration_expression.js
+++ assignments/JS Fundamentals part2/func_declaration_expression.js
@@ -1,10 +1,12 @@
 // Function Declaration
-function percentageOfWorld1(chinaPopulation) {
+function percentageOfWorld1(countryPopulation) {
     let population = 7900;
-    let percentage = (chinaPopulation / population) * 100;
+    let percentage = (countryPopulation / population) * 100;
     return percentage;
 }
-
+console.log(`
+Function Declaration ~>
+`);
 console.log(`${percentageOfWorld1(10)}`);
 console.log(`${percentageOfWorld1(1441)}`);
 console.log(`${percentageOfWorld1(332)}`);
@@ -12,3 +14,15 @@ console.log(`${percentageOfWorld1(332)}`);
 
 
 // Function Expression
+let percentageOfWorld2 = function(countryPopulation1) {
+    let population1 = 7900;
+    let percentage1 = (countryPopulation1 / population1) * 100;
+    return percentage1;
+}
+
+console.log(`
+Function Expression ~>
+`);
+console.log(`${percentageOfWorld2(10)}`);
+console.log(`${percentageOfWorld2(1441)}`);
+console.log(`${percentageOfWorld2(332)}`);
