c27f64b956265ec88cde495a6da821e7767fb9a3
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-08 07:54:58 +0530

    Assignment Completed

--- assignments/JS Fundamentals part2/arrow_function.js
+++ assignments/JS Fundamentals part2/arrow_function.js
@@ -2,6 +2,9 @@
 let population = 7900;
 let percentageOfWorld3 = countryPopulation => (countryPopulation / population) * 100;
 
+console.log(`
+Arrow Function Expression
+`);
 console.log(`${percentageOfWorld3(10)}`);
 console.log(`${percentageOfWorld3(1441)}`);
 console.log(`${percentageOfWorld3(332)}`);
