2dfafc39665ec7cf1644d4d4314eeaa2178b43d9
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-08 07:35:19 +0530

    function declaration added

---
+++ assignments/JS Fundamentals part2/func_declaration_expression.js
@@ -0,0 +1,14 @@
+// Function Declaration
+function percentageOfWorld1(chinaPopulation) {
+    let population = 7900;
+    let percentage = (chinaPopulation / population) * 100;
+    return percentage;
+}
+
+console.log(`${percentageOfWorld1(10)}`);
+console.log(`${percentageOfWorld1(1441)}`);
+console.log(`${percentageOfWorld1(332)}`);
+
+
+
+// Function Expression
