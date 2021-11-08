4fd031f3edb9ffc42f57e95f6da635a2c2fd7032
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-08 18:21:45 +0530

    == vs === operator fixed

--- == vs ===/==vs===.js
+++ == vs ===/==vs===.js
@@ -6,13 +6,10 @@ console.log(favourite);
 console.log(typeof favourite);
 
 if (favourite == 23) {
-    console.log(`Cool! 23 is an amazing number ~ True!(==)`);
+  console.log(`Cool! 23 is an amazing number ~ True!(==)`);
+} else {
+  console.log(`False!(==)`);
 }
-else {
-    console.log(`False!(==)`);
-} 
-
-
 
 // Tripple equals to operator
 // (===)
@@ -22,24 +19,22 @@ console.log(favourite2);
 console.log(typeof favourite2);
 
 if (favourite2 === 5) {
-    console.log(`Cool! 5 is an amazing number ~ True!(===)`);
+  console.log(`Cool! 5 is an amazing number ~ True!(===)`);
+} else {
+  console.log(`False!(===)`);
 }
-else {
-    console.log(`False!(===)`);
-}
-
-
 
 // FIXED!! ~ Tripple equals to operator by adding 'Number' to it
 // (===)
-const favourite3 = Number(prompt(`What's your favourite number BETWEEN 1-10? ~ Fixed`));
+const favourite3 = Number(
+  prompt(`What's your favourite number BETWEEN 1-10? ~ Fixed`)
+);
 
 console.log(favourite3);
 console.log(typeof favourite3);
 
 if (favourite3 === 5) {
-    console.log(`Cool! 5 is an amazing number ~ True!(===) ~ Fixed`);
-}
-else {
-    console.log(`False!(===) ~ Fixed`);
+  console.log(`Cool! 5 is an amazing number ~ True!(===) ~ Fixed`);
+} else {
+  console.log(`False!(===) ~ Fixed`);
 }
