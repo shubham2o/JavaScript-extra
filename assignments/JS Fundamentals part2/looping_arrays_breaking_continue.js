c47d1c90737a0a3889121914ae520cce48d22e75
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-08 14:06:27 +0530

    Assignment Completed

--- assignments/JS Fundamentals part2/looping_arrays_breaking_continue.js
+++ assignments/JS Fundamentals part2/looping_arrays_breaking_continue.js
@@ -20,9 +20,13 @@ console.log(`${percentageOfWorld1(1441, 7900)}%
 
 
 
-for (let i = 0; i < populations.length; i++) {
-    let perc = percentageOfWorld1(populations[i], 7780);
+for (let i = 0; i <= populations.length - 1; i++) {
+    let perc = percentageOfWorld1(populations[i], 7900);
     percentages2.push(perc);
 }
 
 console.log(percentages2);
+console.log(percentages2[0]);
+console.log(percentages2[1]);
+console.log(percentages2[2]);
+console.log(percentages2[3]);
