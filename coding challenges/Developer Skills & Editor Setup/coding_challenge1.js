4c0b8f2fa31da10bd1d56afcf9034ddf4114fe1d
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-09 10:23:43 +0530

    Test Data 1 passed

--- coding challenges/Developer Skills & Editor Setup/coding_challenge1.js
+++ coding challenges/Developer Skills & Editor Setup/coding_challenge1.js
@@ -1,21 +1,14 @@
 // Test Data 1
-let temperature = [17, 21, 23];
+const temperature = [17, 21, 23];
 
-function printForecast(arr) {
-  console.log(`${temperature[0]}℃ in 1 days... ${temperature[1]}℃ in 2 days... ${temperature[2]}℃ in 3 days...
+console.log(`1> ${temperature[0]}℃ in 1 days... ${temperature[1]}℃ in 2 days... ${temperature[2]}℃ in 3 days...
 `);
-  return arr;
-}
 
-printForecast();
-
-// Test Data 2
-temperature = [12, 5, -5, 0, 4];
-
-function printForecast(arr) {
-  console.log(`${temperature[0]}℃ in 1 days... ${temperature[1]}℃ in 2 days... ${temperature[2]}℃ in 3 days... ${temperature[3]}℃ in 4 days... ${temperature[4]}℃ in 5 days...
-`);
-  return arr;
-}
-
-printForecast();
+const printForecast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `${arr[i]}℃ `;
+  }
+  console.log(str);
+};
+printForecast(temperature);
