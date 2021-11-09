9ab1e5439ab49cf2a887b44e62e1ffbd8490fe59
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-09 09:59:38 +0530

    Test Data 1 passed

---
+++ coding challenges/Developer Skills & Editor Setup/coding_challenge1.js
@@ -0,0 +1,10 @@
+// Test Data 1
+let temperature = [17, 21, 23];
+
+function printForecast(arr) {
+  console.log(`${temperature[0]}℃ in 1 days... ${temperature[1]}℃ in 2 days... ${temperature[2]}℃ in 3 days...
+`);
+  return arr;
+}
+
+printForecast();
