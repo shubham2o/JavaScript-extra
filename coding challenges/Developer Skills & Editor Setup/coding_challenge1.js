e40e793c8ac8b4841f3b678c1955f4d6b97cee95
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-09 10:25:16 +0530

    Test Data 1 passed

--- coding challenges/Developer Skills & Editor Setup/coding_challenge1.js
+++ coding challenges/Developer Skills & Editor Setup/coding_challenge1.js
@@ -7,7 +7,7 @@ console.log(`1> ${temperature[0]}℃ in 1 days... ${temperature[1]}℃ in 2 days
 const printForecast = function (arr) {
   let str = "";
   for (let i = 0; i < arr.length; i++) {
-    str = str + `${arr[i]}℃ `;
+    str = str + `${arr[i]}℃ in ${i + 1} days... `;
   }
   console.log(str);
 };
