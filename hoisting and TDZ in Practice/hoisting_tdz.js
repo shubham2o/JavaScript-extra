fb1c3d0f0d26c38d5dd1c26bed0a983ad887c6dd
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-12 00:14:20 +0530

    hoisting with functions added

--- hoisting and TDZ in Practice/hoisting_tdz.js
+++ hoisting and TDZ in Practice/hoisting_tdz.js
@@ -11,12 +11,12 @@ const year = 2000;
 
 
 
-// Hoisting with functions
+// Hoisting with functions (CONST AND LET)
 console.log(addDec(3, 5));
 /* console.log(addExpr(3, 0));   ~>  Error !!!
 console.log(addArrow(3, 4));   ~>  Error !!! */
 
-// Function Declaration
+// Function Declaration 
 function addDec(a, b) {
     return a + b;
 }
@@ -28,3 +28,23 @@ const addExpr = function(a, b) {
 
 // Arrow function expression
 const addArrow = (a, b) => a + b;
+
+
+
+// Hoisting with functions (VAR)
+console.log(addDec1(3, 5));
+/* console.log(addExpr1(3, 0));   ~>  Error !!!
+console.log(addArrow1(3, 4));   ~>  Error !!! */
+
+// Function Declaration 
+function addDec1(a, b) {
+    return a + b;
+}
+
+// Function Expression
+var addExpr1 = function(a, b) {
+    return a + b;
+}
+
+// Arrow function expression
+var addArrow1 = (a, b) => a + b;
