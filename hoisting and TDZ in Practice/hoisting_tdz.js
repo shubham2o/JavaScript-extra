09e61c78a89e3a20440524e42f3b76b907274a02
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-12 00:08:43 +0530

    hoisting with functions added

--- hoisting and TDZ in Practice/hoisting_tdz.js
+++ hoisting and TDZ in Practice/hoisting_tdz.js
@@ -1,16 +1,30 @@
 'use strict';
 
-
 // Hoisting with variables
-// console.log(me);   ~>  Error !!!
-// console.log(job);   ~>  Error !!!
-// console.log(year);   ~>  Error !!!
-
-
+/*  console.log(me);   ~>  Error !!!
+    console.log(job);   ~>  Error !!!
+    console.log(year);   ~>  Error !!! */
 
 var me = "shubham";
 let job = "Teacher";
 const year = 2000;
 
 
-// Hoisting with function
+
+// Hoisting with functions
+console.log(addDec(3, 5));
+/* console.log(addExpr(3, 0));   ~>  Error !!!
+console.log(addArrow(3, 4));   ~>  Error !!! */
+
+// Function Declaration
+function addDec(a, b) {
+    return a + b;
+}
+
+// Function Expression
+const addExpr = function(a, b) {
+    return a + b;
+}
+
+// Arrow function expression
+const addArrow = (a, b) => a + b;
