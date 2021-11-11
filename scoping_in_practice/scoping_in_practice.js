4fc6afea9d1e54b1ae70c7a8692b5ef563fed227
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-11 15:55:23 +0530

    scoping in practice added

--- scoping_in_practice/scoping_in_practice.js
+++ scoping_in_practice/scoping_in_practice.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 function calcAge(birthYear) {
     const age = 2037 - birthYear;
 //  console.log(firstName);  ~>  ERROR !!!
@@ -8,15 +10,27 @@ function calcAge(birthYear) {
         const output = `${firstName} ~ You are ${age}, born in ${birthYear}!`;
         console.log(output);
         console.log(age);
+        console.log(millenial);
     }
 
+    if(birthYear >= 1981 && birthYear <= 2000) {
+        var millenial = true;   
+        const str = `Oh, and you're a millenial, ${firstName}`
+        console.log(str);
+    }
+
+//  console.log(str);  ~>  ERROR !!!
+    console.log(millenial);
     printAge();
 //  console.log(output);  ~>  ERROR !!!
     return age;
 }
+
+
 
 const firstName = "Shubham";
 calcAge(2000);
 // console.log(output);  ~>  ERROR !!!
 // console.log(age);  ~>  ERROR !!!
 // printAge();  ~>  ERROR !!!
+// console.log(millenial);  ~>  ERROR !!! 
