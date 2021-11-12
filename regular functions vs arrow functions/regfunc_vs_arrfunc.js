5fffb21fa1fe86e4ada3632df4a5e18a1eeb9001
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-12 14:59:29 +0530

    Regular and Arrow function added

---
+++ regular functions vs arrow functions/regfunc_vs_arrfunc.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var firstName = "Matilda";
+
+const shubham = {
+    firstName : "Shubham",
+    year : 2000,
+    calcAge : function() {
+        console.log(this);
+        console.log(2037 - this.year);
+    },
+    // Arrow Function
+    greetArr : () => {  
+        console.log(this);
+        console.log(`Hey ${this.firstName}`);
+    },
+    // Regualar Function
+    greetReg : function () {
+        console.log(this);
+        console.log(`Hey ${this.firstName}`);
+    }
+};
+
+shubham.greetArr();
+shubham.greetReg();
