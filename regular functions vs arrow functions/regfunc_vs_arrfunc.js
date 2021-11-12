b2f6a7caaa93854e34afe8faccc7c75cc19af00f
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-12 15:17:58 +0530

    Function within a function - inherited added

--- regular functions vs arrow functions/regfunc_vs_arrfunc.js
+++ regular functions vs arrow functions/regfunc_vs_arrfunc.js
@@ -1,6 +1,8 @@
 "use strict";
 
+
 var firstName = "Matilda";
+
 
 const shubham = {
     firstName : "Shubham",
@@ -9,6 +11,16 @@ const shubham = {
     calcAge : function() {
         console.log(this);
         console.log(2037 - this.year);
+
+        // Function within a function ~ Inherited
+        const self = this;                          // self or that method used but we can also use arrow method in this case
+        const isMillenial = function() {
+/*(ERROR!)  console.log(this);
+            console.log(this.year >= 1981 && this.year <= 2000); */
+            console.log(self);
+            console.log(self.year >= 1981 && self.year <= 2000);
+        };
+        isMillenial();
     },
     // Arrow Function
     greetArr : () => {  
@@ -24,3 +36,4 @@ const shubham = {
 
 shubham.greetArr();
 shubham.greetReg();
+shubham.calcAge();
