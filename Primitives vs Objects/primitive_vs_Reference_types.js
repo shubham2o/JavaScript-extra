b98910eefdf6d377c182e3034bae412e31ef0f16
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-12 15:56:39 +0530

    Objects added

---
+++ Primitives vs Objects/primitive_vs_Reference_types.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+
+
+// Objects
+const me = {
+    name : "Shubham",
+    age : 30
+};
+const friend = me;
+friend.age = 21;
+console.log(`Me ~ ${me.age}`);
+console.log(`Friend ~ ${friend.age}`);
