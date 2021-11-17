cb58a03099ec3e283ecfd0e37fafcd07f758c131
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-17 23:00:42 +0530

    short circuiting.js completed

---
+++ short circuiting/short_circuiting.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const restaurant = {
+    name : "Classico Italiano",
+    location : "Via Angelo Tavanti 23, Firenze, Italy",
+    categories : ["Italian", "Pizzeria", "Vegeterian", "Organic"],
+    starterMenu : ["Focaccia", "Bruchetta", "Garlic Bread", "Caprese Salad"],
+    mainMenu : ["Pizza", "Pasta", "Risotto"],
+    openingHours : {
+        thu : {
+            open : 12,
+            close : 22,
+        },
+        fri : {
+            open : 11, 
+            close : 23,
+        },
+        sat : {
+            open : 0, // Open 24 Hours
+            close : 24,
+        },
+    },
+    order : function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    orderDelivery : function ({starterIndex, mainIndex, time, address}) {
+        console.log(`Order Received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+};
+
+
+
+/* OR (||)
+Use ANY data type, return ANY data type
+short-circuiting */
+console.log(3 || "Shubham");
+console.log("" || "Shubham");
+console.log(true || 0);
+console.log(undefined || null);
+
+// First truthy value will be the answer
+console.log(undefined || 0 || "" || "Shubham" || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+
+
+// AND (&&)
+console.log("======AND======");
