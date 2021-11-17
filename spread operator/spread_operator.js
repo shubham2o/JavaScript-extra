57a7321a12f773a596e0c4614b6c76b52354cd28
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-17 11:27:21 +0530

    spread operator added

--- spread operator/spread_operator.js
+++ spread operator/spread_operator.js
@@ -1,5 +1,33 @@
 "use strict";
 
+const restaurant = {
+    name : "Classico Italiano",
+    location : "Via Angelo Tavanti 23, Firenze, Italy",
+    categories : ["Italian", "Pizzeria", "Vegeterian", "Organic"],
+    starterMenu : ["Focaccia", "Bruchetta", "Garlic Bread", "Caprese Salad"],
+    mainMenu : ["Pizza", "Pasta", "Risotto"],
+    openingHours : {
+        thu : {
+            open : 12,
+            close : 22,
+        },
+        fri : {
+            open : 11, 
+            close : 23,
+        },
+        sat : {
+            open : 0, // Open 24 Hours
+            close : 24,
+        },
+    },
+    order : function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    orderDelivery : function ({starterIndex, mainIndex, time, address}) {
+        console.log(`Order Received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+};
+
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -8,3 +36,4 @@ const newArr = [1, 2, ...arr];
 console.log(newArr);
 
 console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
