2e8038a3ec21d9739e4c852f6d89a79a0d3029c0
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-19 02:29:38 +0530

    OR || logical_assignment operator added

--- logical assignment operators/logical_assignment_operator.js
+++ logical assignment operators/logical_assignment_operator.js
@@ -1,14 +1,13 @@
 "use strict";
 
-
+// OR || Assignment Operator
 const rest1 = {
     name : "Capri",
     numGuests: 20,
 };
 
 // rest1.numGuests = rest1.numGuests || 10;
-
-// Logical Assignment Operator (can also be written as)
+// OR || Logical Assignment Operator (can also be written as)
 rest1.numGuests ||= 10;
 
 console.log(rest1);
@@ -21,8 +20,7 @@ const rest2 = {
 };
 
 // rest2.numGuests = rest2.numGuests || 10;
-
-// Logical Assignment Operator (can also be written as)
+// OR || Logical Assignment Operator (can also be written as)
 rest2.numGuests ||= 10;
 
 console.log(rest2);
