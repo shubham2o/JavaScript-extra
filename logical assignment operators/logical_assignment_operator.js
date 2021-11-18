120af5dd626bd51a1b56c30ec3b6d173def66aef
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-19 02:55:19 +0530

    AND && logical_assignment operator added

--- logical assignment operators/logical_assignment_operator.js
+++ logical assignment operators/logical_assignment_operator.js
@@ -36,3 +36,24 @@ const rest4 = {
 };
 rest4.numGuests ||= 10;
 console.log(rest4);
+
+
+
+// AND && Assignment Operator (null or undefined)
+const rest5 = {
+    name : "Capri",
+    owner : "Giovanni Rossi",
+};
+// rest5.owner = rest5.owner && "<ANONYMOUS>";
+// AND && Logical Assignment Operator (can also be written as)
+rest5.owner &&= "<ANONYMOUS>";
+console.log(rest5);
+
+const rest6 = {
+    name : "Capri",
+    numGuests :0,
+};
+// rest6.owner = rest6.owner && "<ANONYMOUS>";
+// AND && Logical Assignment Operator (can also be written as)
+rest6.owner &&= "<ANONYMOUS>";
+console.log(rest6);
