672f2c801515fb94df468c69c8352842affc0eef
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-25 20:03:14 +0530

    working with the strings part 2 completed

--- working with strings part 2/working_w_strings_p2.js
+++ working with strings part 2/working_w_strings_p2.js
@@ -30,3 +30,47 @@ console.log(trimmedEmail);
 const normalizedEmail = loginEmail.toLowerCase().trim();      // trim() removes all the whitespace (extra-space ~ left, right, center space) that is within the variable.
 console.log(normalizedEmail);
 console.log(email === normalizedEmail); 
+
+console.log(`
+4 ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ >`);
+// Replacing
+const priceGB = "288,97£";
+const priceUS = priceGB.replace("£", "$");
+console.log(priceUS);
+
+const announcement = `All passengers come to boarding door 23, Boarding door 23!`;
+console.log(announcement.replace("door", "gate"));
+console.log(announcement.replaceAll("door", "gate"));
+console.log(announcement.replace(/door/g, "gate"));           // g here means global
+
+console.log(`
+5 ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ >`);
+// Booleans
+const plane = "Airbus A320neo";
+console.log(plane.includes("A320"));
+console.log(plane.includes("Boeing"));
+console.log(plane.startsWith("Air"));
+console.log(plane.startsWith("A32"));
+console.log(plane.endsWith("neo"));
+console.log(plane.endsWith("bus"));
+
+if (plane.startsWith("Air") && plane.endsWith("neo")) {
+    console.log(`Part of the NEW AIRbus family`);
+}
+
+console.log(`
+6 ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ >`);
+// Practice Exercise
+const checkBaggage = function(items) {
+    const baggage = items.toLowerCase();
+    if(baggage.includes("knife") || baggage.includes("gun")) {
+        console.log(`You are NOT allowed on board!`);
+    }
+    else {
+        console.log(`Welcome aboard :>`);
+    }
+};
+
+checkBaggage(`I have a laptop, some Food and a pocket Knife`);
+checkBaggage(`Socks and camera`);
+checkBaggage(`Got some snacks and a gun for protection`);
