1093c017e13a6c20ca9cfde66526eef71bbecf36
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-26 11:22:37 +0530

    working with the strings part 3 completed

--- working with strings part 3/working_w_strings_p3.js
+++ working with strings part 3/working_w_strings_p3.js
@@ -28,6 +28,9 @@ const capitalizeName = function (name) {
 
     for (const n of names) {
         namesUpper.push(n[0].toUpperCase() + n.slice(1));
+
+        // Above can also be written as
+        // namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
     }
 
     console.log(namesUpper.join(" "));
@@ -35,3 +38,38 @@ const capitalizeName = function (name) {
 capitalizeName("stephen curry");
 capitalizeName("klay thompson");
 capitalizeName("draymond green");
+
+console.log(`
+5 ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ >`);
+// Padding
+const message = "Go to gate 23!";
+console.log(message.padStart(25, "+").padEnd(30, "*"));     // padEnd(30) will add up 5 * in the end because padStart(25) had already made the length of the variable 25. So, 30 - 25 = 5.
+console.log(message.padStart(25, "+").padEnd(30, "*").length);
+
+console.log("Shubham".padStart(25, "+").padEnd(30, "*"));
+console.log(message.padStart(25, "+").padEnd(30, "*").length);
+
+console.log(`
+6 ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ >`);
+const maskCreditCard = function (number) {
+    const str = number + "";
+    const last = str.slice(-4);
+    return last.padStart(str.length, "*");
+}
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard("334859493847755774747"));
+
+console.log(`
+7 ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ >`);
+// Repeat
+const message2 = "Bad weather... All Departures Delayed...";
+console.log(message2.repeat(5)); 
+
+const planesInLine = function (n) {
+    console.log(`There are ${n} planes in line ${"PLANE ".repeat(n)}`);
+}
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
