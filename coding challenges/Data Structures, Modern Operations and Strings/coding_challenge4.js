978bb859a7f3b272122fac66575a2ff6cef443ac
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-26 12:14:07 +0530

    coding challenge 4 not completed

--- coding challenges/Data Structures, Modern Operations and Strings/coding_challenge4.js
+++ coding challenges/Data Structures, Modern Operations and Strings/coding_challenge4.js
@@ -5,4 +5,4 @@ document.body.append(document.createElement('button'));
 document.querySelector("button").addEventListener("click", function() {
     const text = document.querySelector("textarea").value;
     console.log(text);
-})
+}) 
