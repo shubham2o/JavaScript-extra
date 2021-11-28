8daa4a77159590300466e2c47aa9849c3914a569
shubham2o <shubhamsharma.ha@gmail.com>
2021-11-28 19:16:38 +0530

    call and apply methods completed

---
+++ call and apply methods/call_and_apply_methods.js
@@ -0,0 +1,46 @@
+const lufthansa = {
+    airline : "Lufthansa",
+    iataCode : "LH",
+    bookings : [],
+    // book : function() {}
+    book(flightNum, name) {
+        console.log(
+            `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`
+        );
+        this.bookings.push({flight: `${this.iataCode}${flightNum}`, name});
+    },
+};
+
+lufthansa.book(250, "Shubham");
+lufthansa.book(300, "Stephen Curry");
+console.log(lufthansa);
+
+const book = lufthansa.book;
+
+const eurowings = {
+    airline : "Eurowings",
+    iataCode : "EW",
+    bookings : [],
+}
+
+// Does not work
+// book(011, "Klay Thompson");
+
+console.log(`||||||||||||||||||||||||||||||||||||||||||||||||||||||`);
+book.call(eurowings, 111, "Klay Thompson");
+console.log(eurowings);
+
+console.log(`||||||||||||||||||||||||||||||||||||||||||||||||||||||`);
+book.call(lufthansa, 233, "Draymond Green")
+console.log(lufthansa);
+
+const swiss = {
+    airline : "Swiss Airlines",
+    iataCode : "LX",
+    bookings : [],
+}
+
+console.log(`||||||||||||||||||||||||||||||||||||||||||||||||||||||`);
+book.call(swiss, 222, "Andrew Wiggins");
+book.call(swiss, 333, "Jordan Poole");
+console.log(swiss);
