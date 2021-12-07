54eaa33e6c3631231fb564916268e04e78c9318d
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-07 15:17:34 +0530

    The filter method completed

--- data transformations - map, filter, reduce/map_filter_reduce.js
+++ data transformations - map, filter, reduce/map_filter_reduce.js
@@ -2,7 +2,6 @@
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const euroToUsd = 1.1;
 
-
 /*
 const movementsUSD = movements.map(function (mov) {
     return mov * euroToUsd;
@@ -27,13 +26,39 @@ console.log(``);
 const movementsDescriptions = movements.map((mov, i) => 
 // {
     ` Movement ${i + 1} : You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(mov)}`
-
-//  if (mov > 0) {
-//      return ` Movement ${i + 1} : You deposited ${mov}`;
-//  }
-//  else {
-//      return ` Movement ${i + 1} : You withdrew ${Math.abs(mov)}`;
-//  }
-// }
+/*
+    if (mov > 0) {
+        return ` Movement ${i + 1} : You deposited ${mov}`;
+    }
+    else {
+        return ` Movement ${i + 1} : You withdrew ${Math.abs(mov)}`;
+    }
+*/
 );
 console.log(`4> ` + movementsDescriptions);
+console.log(``);
+
+
+// FILTER METHOD
+const deposits = movements.filter(function (mov) {
+    return mov > 0;
+});
+console.log(`5> ` + movements);
+console.log(``);
+console.log(`6> ` + deposits);
+console.log(``);
+
+
+const depositsFor = [];
+for (const mov of movements) {
+    if (mov > 0) {
+        depositsFor.push(mov)
+    }
+}
+console.log(`7> ` + depositsFor);
+console.log(``);
+
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(`8> ` + withdrawals);
+console.log(``);
