6c38e0eb14287667dee6ae0260a197cf005767aa
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-09 20:08:34 +0530

    numeric separators completed

---
+++ numeric separators/numeric_separators.js
@@ -0,0 +1,20 @@
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(`1> ` + diameter);
+
+const price = 345_99;
+console.log(`2> ` + price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+// const PI = _3.1415;
+// const PI1 = 3._1415;
+// const PI2 = 3.__1415;
+const PI3 = 3.14_15;
+
+console.log(Number("230000"));
+console.log(Number("230_000"));
+
+console.log(parseInt("230000"));
+console.log(parseInt("230_000"));
