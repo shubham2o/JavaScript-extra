f823a89cdbb02791fb97149dda2d04dcac4940ba
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-11 01:19:25 +0530

    constructor func-new operator completed

---
+++ constructor functions and the new operator/constructor_functions_new_operator.js
@@ -0,0 +1,42 @@
+"use strict";
+/*
+In OOPs ~ 
+=> Constructor function always start with a capital letter.
+=> The only difference between a regular function and a constructor function is that 
+    we call the constructor function using the new keyword.
+*/
+
+const Person = function(firstName, birthYear) {
+    // Instance Properties
+    this.firtName = firstName;
+    this.birthYear = birthYear;
+    /* 
+    // You should never create a function inside a constructor function, NEVER do this!
+    this.calcAge = function() {
+        console.log(2037 - this.birthYear);
+    };
+    */
+};
+
+const jonas = new Person("Jonas", 1991);
+const shubham = new Person("Shubham", 2000);
+console.log(jonas, shubham);
+console.log(``);
+
+/*
+1. New {} is created
+2. function is called, this = {}
+3. {} linked to prototype
+4. function automatically return {}
+*/
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
+console.log(matilda, jack);
+console.log(``);
+
+console.log(jonas instanceof Person, shubham instanceof Person, matilda instanceof Person, jack instanceof Person);
+console.log(``);
+
+const stephen = "Stephen";
+console.log( stephen instanceof Person);
