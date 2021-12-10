c893cf7a69a72bb0e1ec971684d4c0d0f94632df
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-10 13:43:36 +0530

    styles, attributes & classes completed

---
+++ styles, attributes and classes/styles_attributes_classes.js
@@ -0,0 +1,29 @@
+// Styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+/*
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(computedStyle(message).height);
+*/
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 40 + "px";
+
+
+// Attributes
+const logo = document.querySelector(".nav_logo");
+logo.alt = "Beautiful minimalist logo";
+logo.getAttribute("src");
+
+const link = document.querySelector(".twitter-link");
+
+
+// Classes
+logo.classList.add();
+logo.classList.remove();
+logo.classList.toggle();
+logo.classList.contains();
+// Don't use
+logo.className = "shubham";
