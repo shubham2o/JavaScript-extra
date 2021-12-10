92a80c108e44bf885e7e9b79c1d2300a0e766d9d
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-11 03:33:59 +0530

    ES6 classes completed

---
+++ ES6 classes/ES6_classes.js
@@ -0,0 +1,36 @@
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+    constructor(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+
+    // Methods will be added to the .prototype property
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+
+    greet() {
+        console.log(`Hey ${this.firstName}`);
+    }
+}
+
+const jessica = new PersonCl("Jessica", 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function() {
+//     console.log(`Hey ${this.firstName}`);
+// }
+jessica.greet();
+
+/*
+1. Classes are NOT hoisted.
+2. Classes are first-class citizens.
+3. Classes are executed in strict mode.
+*/
