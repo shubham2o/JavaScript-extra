fdd48b3f2a334b48908d6332e3f16c288390bc89
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-11 14:25:45 +0530

    inheritance between classes completed

---
+++ inheritance between classes/inheritance_between_classes.js
@@ -0,0 +1,35 @@
+const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+    // this.firstName = firstName;
+    // this.birthYear = birthYear;
+    Person.call(this, firstName, birthYear);
+    this.course  = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function() {
+    console.log(`My name is ${this.firstName} and I study in ${this.course}`);
+}
+
+const mike = new Student("Mike", 2020, "Computer Science");
+mike.introduce();
+// mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
