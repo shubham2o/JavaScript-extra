9a4376fde69087c7ef3fae7063271df0eaba9ff3
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-15 20:26:50 +0530

    Handling Rejected Promises

--- our first AJAX Call - XMLHttpRequest/script.js
+++ our first AJAX Call - XMLHttpRequest/script.js
@@ -1,5 +1,32 @@
 "use strict";
 
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+const renderCountry = function (data, className = " ") {
+    const html = `
+    <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+    </article>
+    `;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    // countriesContainer.style.opacity = 1;
+};
+
+
+const renderError = function(msg) {
+    countriesContainer.insertAdjacentText("beforeend", msg);
+    // countriesContainer.style.opacity = 1;
+};
+
+
 /* // Our first AJAX Call : XMLHttpRequest
 const getCountryData = function (country) {
     const btn = document.querySelector(".btn-country");
@@ -32,27 +59,6 @@ const getCountryData = function (country) {
 */
 
 // Welcome to callback hell
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
-
-const renderCountry = function (data, className = " ") {
-    const html = `
-    <article class="country ${className}">
-        <img class="country__img" src="${data.flag}" />
-        <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-        </div>
-    </article>
-    `;
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-};
-
-
 const getCountryAndNeighbour = function (country) {
     // AJAX call country 1
     const request = new XMLHttpRequest();
@@ -131,7 +137,10 @@ const getCountryData = function (country) {
 const getCountryData = function (country) {
     // Country 1
     fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(response => response.json())
+    .then(
+        response => response.json(), 
+        // err => alert(err)
+    )
     .then(data => {
         renderCountry(data[0]);
         const neighbour = data[0].borders[0]
@@ -141,8 +150,22 @@ const getCountryData = function (country) {
         // Country 2
         return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
     })
-    .then(response => response.json())
-    .then(data => renderCountry(data, "neighbour")); 
+    .then(
+        response => response.json(),
+        // err => alert(err)
+    )
+    .then(data => renderCountry(data, "neighbour"))
+    .catch(err => {
+        console.error(`${err} !! !! !!`);
+        renderError(`Something went wrong ❗❗❗ ~ ${err.message}. Try again!`);
+    })
+    .finally(() => {
+        countriesContainer.style.opacity = 1;
+    }); 
 };
 
-getCountryData("bharat");
+
+btn.addEventListener("click", function() {
+    getCountryData("bharat");
+});
+
