fdf9e7c945861aec189f7d89c31dd810b7b4fd20
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-15 16:17:29 +0530

    callback hell added

--- our first AJAX Call - XMLHttpRequest/script.js
+++ our first AJAX Call - XMLHttpRequest/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Our first AJAX Call : XMLHttpRequest
+/* // Our first AJAX Call : XMLHttpRequest
 const getCountryData = function (country) {
     const btn = document.querySelector(".btn-country");
     const countriesContainer = document.querySelector(".countries");
@@ -29,17 +29,21 @@ const getCountryData = function (country) {
         countriesContainer.style.opacity = 1;
     });
 };
+*/
 
 
 // Welcome to callback hell
-const renderCountry = function (data) {
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+const renderCountry = function (data, className = " ") {
     const html = `
-    <article class="country">
+    <article class="country ${className}">
         <img class="country__img" src="${data.flag}" />
         <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
             <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
         </div>
@@ -47,11 +51,11 @@ const renderCountry = function (data) {
     `;
     countriesContainer.insertAdjacentHTML("beforeend", html);
     countriesContainer.style.opacity = 1;
-}
+};
 
 
-// AJAX call country 1
 const getCountryAndNeighbour = function (country) {
+    // AJAX call country 1
     const request = new XMLHttpRequest();
     request.open("GET", `https://restcountries.com/v2/name/${country}`);
     request.send();
@@ -63,25 +67,37 @@ const getCountryAndNeighbour = function (country) {
         // Render country 1
         renderCountry(data);
 
-        // Get neighbour country (1)
+        // Get neighbour country (2)
         const [neighbour] = data.borders; 
 
+        if (!neighbour) return;
+
         // AJAX call country 2
-        if (!neighbour) {
-            return;
-            const request2 = new XMLHttpRequest();
-            request.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
-            request2.send();
-            
-            request2.addEventListener("load", function () {
-                const data2 = JSON.parse(this.responseText);
-                console.log(data2);
+        const request2 = new XMLHttpRequest();
+        request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+        request2.send();
+        
+        request2.addEventListener("load", function () {
+            const data2 = JSON.parse(this.responseText);
+            console.log(data2);
 
-                renderCountry(data2);
-            })
-        }
+            renderCountry(data2, "neighbour");
+        });
     });
-}   
+}; 
 
+getCountryAndNeighbour("bharat");
 
-getCountryAndNeighbour("portugal");
+// Callback Hell
+setTimeout(() => {
+    console.log(`1 second passed`);
+    setTimeout(() => {
+        console.log(`2 second passed`);
+        setTimeout(() => {
+            console.log(`3 second passed`);
+            setTimeout(() => {
+                console.log(`4 second passed`);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000)
