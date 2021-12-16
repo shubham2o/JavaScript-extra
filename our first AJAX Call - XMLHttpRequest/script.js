3a8a447ec9a918b1ed871a2687d1aa797f66b8e4
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-17 00:09:44 +0530

    Event Loop completed

--- our first AJAX Call - XMLHttpRequest/script.js
+++ our first AJAX Call - XMLHttpRequest/script.js
@@ -252,18 +252,22 @@ btn.addEventListener("click", function() {
 });
 
 
-// Event Loop
+// Event Loop - explained below is that you cannot really
+// do high precision things using JavaScript timers.
 console.log("Test start");
+
 setTimeout(() => 
-    console.log(`0 sec timer`), 0)
-    Promise.resolve(`Resolved promise 1`)
+    console.log(`0 sec timer`), 0);
+
+Promise
+    .resolve(`Resolved promise 1`)
     .then(res => console.log(res));
 
-Promise.resolve(`Resolved promise 2`).then(res => {
-    for (let i = 0; i < 10000000; i++) {
-        
-    }
+Promise
+    .resolve(`Resolved promise 2`)
+    .then(res => {
+    for (let i = 0; i < 1000000000; i++) {}
     console.log(res);
-})
+});
 
 console.log("Test end");
