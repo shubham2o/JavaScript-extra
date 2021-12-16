b745d28d12a375e3e1b1e088fd1f3f85f1d02c89
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-16 23:59:10 +0530

    Event Loop added

--- our first AJAX Call - XMLHttpRequest/script.js
+++ our first AJAX Call - XMLHttpRequest/script.js
@@ -257,6 +257,11 @@ console.log("Test start");
 setTimeout(() => 
     console.log(`0 sec timer`), 0)
     Promise.resolve(`Resolved promise 1`)
-    .then(res => console.log(res)
-);
+    .then(res => console.log(res));
+
+Promise.resolve(`Resolved promise 2`).then(res => {
+    for (let i = 0; )
+    console.log(res);
+})
+
 console.log("Test end");
