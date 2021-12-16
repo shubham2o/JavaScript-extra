764cb82e16c462f3a279332446ae26bf859ea8c0
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-17 02:39:20 +0530

    Building a simple promise completed

--- our first AJAX Call - XMLHttpRequest/script.js
+++ our first AJAX Call - XMLHttpRequest/script.js
@@ -251,7 +251,7 @@ btn.addEventListener("click", function() {
     getCountryData("australia");
 });
 
-
+/*
 // Event Loop - explained below is that you cannot really
 // do high precision things using JavaScript timers.
 console.log("Test start");
@@ -271,3 +271,57 @@ Promise
 });
 
 console.log("Test end");
+*/
+
+// Building a simple promise
+const lotteryPromise = new Promise(function(resolve, reject) {
+    console.log(`$$$ Lottery draw is happening $$$`);
+    setTimeout(function() {
+        if (Math.random() >= 0.5) {
+            resolve(`:> You WIN :> $$$$$$$$$ :>`);
+        }
+        else {
+            // reject(`:< You LOST your money :< !!!!!!!! :<`);
+            reject(new Error(`:< You LOST your money :< !!!!!!!! :<`));
+        }
+    },2000)
+});
+
+lotteryPromise
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
+
+// Promisifying setTimeout
+const wait = function(seconds) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+wait(1)
+    .then(() => {
+        console.log("I waited for 1 seconds.");
+        return wait(1);
+    })
+    .then(() => {
+        console.log("I waited for 2 seconds.");
+        return wait(1);
+    })
+    .then(() => {
+        console.log("I waited for 3 seconds.");
+        return wait(1);
+    })
+    .then(() =>  {
+        console.log("I waited for 4 seconds.");
+        return wait(1);
+    });
+
+
+Promise
+    .resolve("abc")
+    .then(x => console.log(x));
+
+Promise
+    .reject(new Error("Problem!"))
+    .catch(x => console.error(x));
