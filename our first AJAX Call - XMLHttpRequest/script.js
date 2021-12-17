0aa9aa8ab2e2acaa2a277c06e0c17af54816d1da
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-17 16:22:36 +0530

    Running Promises in Parallel completed

--- our first AJAX Call - XMLHttpRequest/script.js
+++ our first AJAX Call - XMLHttpRequest/script.js
@@ -246,12 +246,12 @@ const getCountryData = function (country) {
         countriesContainer.style.opacity = 1;
     }); 
 };
-
+/*
 btn.addEventListener("click", function() {
     getCountryData("australia");
 });
 
-/*
+
 // Event Loop - explained below is that you cannot really
 // do high precision things using JavaScript timers.
 console.log("Test start");
@@ -415,17 +415,17 @@ const whereAmI = async function(country) {
 // whereAmI();
 // console.log("FIRST");
 
-
+/*
 // Returning Values from Async Functions
 console.log(`1: Will get location`);
 const city = whereAmI();
 console.log(city);
-/*
+
 whereAmI()
     .then(city => console.log(`2: ${city}`))
     .catch(err => console.error(`2: ${err.message}`))
     .finally(() => console.log(`3: Finished getting location`));
-*/
+
 (async function() {
     try {
         const city = await whereAmI();
@@ -436,3 +436,27 @@ whereAmI()
     }
     console.log(`3: Finished getting location`);
 })();
+*/
+
+// Running Promises in Parallel
+const get3Countries = async function(c1, c2, c3) {
+    try {
+        // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+        // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+        // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+
+        // console.log([data1.capital, data2.capital, data3.capital]);
+
+        const data = await Promise.all([
+        getJSON(`https://restcountries.com/v2/name/${c1}`),
+        getJSON(`https://restcountries.com/v2/name/${c2}`),
+        getJSON(`https://restcountries.com/v2/name/${c3}`),
+        ]);
+    console.log(data.map(d => d[0].capital));
+    } 
+    catch (err) {
+        console.log(err);
+    }
+};
+
+get3Countries("bharat", "usa", "australia");
