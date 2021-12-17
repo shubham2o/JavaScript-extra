9799d9cf0ead382825bbe70c5e6a05d94e5e8960
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-18 02:19:41 +0530

    the module pattern completed

---
+++ the module pattern/the_module_pattern.js
@@ -0,0 +1,28 @@
+const ShoppingCart2 = (function () {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 237;
+    const totalQuantity =23;
+
+    const addToCart = function (product, quantity) {
+        cart.push({product, quantity});
+        console.log(`${quantity} ${product} added to cart.`);
+    };
+
+    const orderStock = function (product, quantity) {
+        console.log(`${quantity} ${product} ordered from supplier.`);
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+        // shippingCost,
+    };
+})();
+
+ShoppingCart2.addToCart("apple", 4);
+ShoppingCart2.addToCart("pizza", 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
