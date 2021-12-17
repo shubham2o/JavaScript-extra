b193ae1afec820f6448b048a0460bb276713f3e0
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-18 01:13:38 +0530

    exporting importing ES6 module added

---
+++ exporting and importing in ES6 modules/script.js
@@ -0,0 +1,4 @@
+// Importing module
+import { addToCart } from "./shoppingCart";
+
+console.log("Importing Module");
---
+++ exporting and importing in ES6 modules/shoppingCart.js
@@ -0,0 +1,10 @@
+// Exporting module
+console.log("Exporting Module");
+
+const shippingCost = 10;
+const cart = [];
+
+const addToCart = function (product, quantity) {
+    cart.push( {product, quantity});
+    console.log(`${quantity} ${product} added to cart`);
+};
